dc011bae0bf31e1a7d5d00b64b3a6adb3f5f348a
david-kucera <kucera.david2002@gmail.com>
2024-10-09 17:11:29 +0200

    Rozpracovanie funkcie insert

--- modules/DataStructures/KDTree.cs
+++ modules/DataStructures/KDTree.cs
@@ -4,16 +4,14 @@
 	{
 		#region Properties
 		public Node<T> Root { get; set; }
-		public int TotalTreeDimension { get; set; }
-		public int CurrentTreeDimension { get; set; }
+		public int TreeDimension { get; set; }
 		public int Count { get; set; }
 		#endregion //Properties
 
 		#region Constructor
-		public KdTree(int totalTreeDimension)
+		public KdTree(int treeDimension)
 		{
-			TotalTreeDimension = totalTreeDimension;
-			CurrentTreeDimension = 0;
+			TreeDimension = treeDimension;
 			Count = 0;
 			Root = null!;
 		}
@@ -22,57 +20,85 @@
 		#region Public functions
 		public void Insert(T data)
 		{
+			int depth = 0;
 			if (Root == null!)
 			{
 				Root = new Node<T>
 				{
 					Data = data,
 					Parent = null!,
-					Dimension = CurrentTreeDimension // 0
+					Dimension = depth // 0
 				};
 				Count++;
-				CurrentTreeDimension++;
+				depth++;
 
-				int position = Root.CompareTo(data, CurrentTreeDimension);
-				if (position == -1)
+				int position = Root.CompareTo(data, depth % TreeDimension);
+				if (position == -1 || position == 0)
 				{
 					Root.Left = new Node<T>
 					{
 						Data = data,
 						Parent = Root,
-						Dimension = CurrentTreeDimension++
+						Dimension = depth % TreeDimension // 1
 					};
-					Count++;
 				}
-				else if (position == 1)
+				else
 				{
 					Root.Right = new Node<T>
 					{
 						Data = data,
 						Parent = Root,
-						Dimension = CurrentTreeDimension++
+						Dimension = depth % TreeDimension // 1
 					};
-					Count++;
+				}
+				Count++;
+				return;
+			}
+
+			for (int i = 0; i < TreeDimension; i++)
+			{
+				Node<T> currentNode = Root;
+				Node<T> parentNode = null!;
+				depth = 0;
+				int currentTreeDimension = i;
+
+				while (currentNode != null!)
+				{
+					parentNode = currentNode;
+
+					int position = currentNode.CompareTo(data, currentTreeDimension);
+					if (position == -1 || position == 0)
+					{
+						currentNode = currentNode.Left;
+					}
+					else
+					{
+						currentNode = currentNode.Right;
+					}
+
+					depth++;
+					currentTreeDimension = depth % TreeDimension;
+				}
+
+				Node<T> newNode = new Node<T>
+				{
+					Data = data,
+					Parent = parentNode,
+					Dimension = depth % TreeDimension
+				};
+
+				//currentTreeDimension = depth % TreeDimension;
+				int newPosition = parentNode.CompareTo(data, currentTreeDimension);
+				if (newPosition == -1 || newPosition == 0)
+				{
+					parentNode.Left = newNode;
 				}
 				else
 				{
-					Root.Left = new Node<T>
-					{
-						Data = data,
-						Parent = Root,
-						Dimension = CurrentTreeDimension++
-					};
-					Count++;
+					parentNode.Right = newNode;
 				}
-
-				CurrentTreeDimension--;
+				Count++;
 			}
-			else
-			{
-				
-				
-			}
-			
 		}
 
 		public void Delete(T data)
--- modules/TestApp/Program.cs
+++ modules/TestApp/Program.cs
@@ -10,13 +10,24 @@ namespace TestApp
 			KdTree<Nehnutelnost> nehnutelnosti = new KdTree<Nehnutelnost>(2);
 			//KdTree<Parcela> parcely = new KdTree<Parcela>(2);
 			//KdTree<GeoObjekt> objekty = new KdTree<GeoObjekt>(2);
-			GPSPos pozicia1 = new GPSPos('N',17,'E', 20);
-			GPSPos pozicia2 = new GPSPos('N', 11, 'E', 10);
-			Nehnutelnost nehnutelnost = new Nehnutelnost();
-			nehnutelnost.Pozicie[0] = pozicia1;
-			nehnutelnost.Pozicie[1] = pozicia2;
-			
-			nehnutelnosti.Insert(nehnutelnost);
+
+
+			int[] sur1D = new[] { 94, 6, 123, 101, 179, 35, 85, 91, 48, 144 };
+			int[] sur1S = new[] { 157, 19, 14, 56, 17, 174, 95, 83, 166, 92 };
+
+			int[] sur2D = new[] { 77, 24, 167, 54, 104, 83, 48, 62, 24, 47 };
+			int[] sur2S = new[] { 142, 43, 5, 100, 79, 94, 106, 55, 49, 92 };
+
+			for (int i = 0; i < sur1D.Length; i++)
+			{
+				GPSPos pozicia1 = new GPSPos('N', sur1S[i], 'E', sur1D[i]);
+				GPSPos pozicia2 = new GPSPos('N', sur2S[i], 'E', sur2D[i]);
+				Nehnutelnost nehnutelnost = new Nehnutelnost();
+				nehnutelnost.Pozicie[0] = pozicia1;
+				nehnutelnost.Pozicie[1] = pozicia2;
+				nehnutelnosti.Insert(nehnutelnost);
+			}
+
 			Console.WriteLine(nehnutelnosti.Count);
 			while (true)
 			{
