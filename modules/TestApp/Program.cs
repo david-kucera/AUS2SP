de1535173126646aa6bbfb0b4c11d6636e91aa3e
david-kucera <kucera.david2002@gmail.com>
2024-10-13 08:31:23 +0200

    Úprava testovacej konzolovej aplikácie

--- modules/TestApp/Program.cs
+++ modules/TestApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using DataStructures;
+﻿using System.Diagnostics;
+using DataStructures;
 using DataStructures.Data;
 
 namespace TestApp
@@ -7,40 +8,40 @@ namespace TestApp
 	{
 		static void Main(string[] args)
 		{
-			KdTree<Nehnutelnost> nehnutelnosti = new KdTree<Nehnutelnost>(2);
-			//KdTree<Parcela> parcely = new KdTree<Parcela>(2);
-			//KdTree<GeoObjekt> objekty = new KdTree<GeoObjekt>(2);
+			KdTree<GeoObjekt> objekty = new KdTree<GeoObjekt>(2);
 
 			Console.WriteLine("Generované nehnuteľnosti");
+			Random rnd = new Random();
 			DataGenerator generator = new DataGenerator(0);
-			for (int i = 0; i < 5; i++)
+
+
+			int pocetGenerovanych = 10000;
+			int pocetNehnutelnosti = 0;
+			int pocetParcel = 0;
+			Stopwatch stopWatch = new Stopwatch();
+			stopWatch.Start();
+			for (int i = 0; i < pocetGenerovanych; i++)
 			{
-				var nehnutelnost = generator.GenerateNehnutelnost();
-				Console.WriteLine(nehnutelnost.ToString() + "\n");
-				nehnutelnosti.Insert(nehnutelnost);
+				GeoObjekt objekt;
+				if (rnd.NextSingle() < 0.5)
+				{
+					objekt = generator.GenerateNehnutelnost();
+					pocetNehnutelnosti++;
+				}
+				else
+				{
+					objekt = generator.GenerateParcela();
+					pocetParcel++;
+				}
+				
+				objekty.Insert(objekt);
 			}
+			stopWatch.Stop();
+			Console.WriteLine("Čas generovania a vkladania " + pocetGenerovanych + ": " + stopWatch.ElapsedMilliseconds + " ms");
+			Console.WriteLine("Počet parciel: " + pocetParcel);
+			Console.WriteLine("Počet nehnuteľností: " + pocetNehnutelnosti);
 
-			//int[] sur1S = { 157, 19, 14, 56, 17, 174, 95, 83, 166, 92 };
-			//int[] sur1D = { 94, 6, 123, 101, 179, 35, 85, 91, 48, 144 };
-
-			//int[] sur2S = { 142, 43, 5, 100, 79, 94, 106, 55, 49, 92 };
-			//int[] sur2D = { 77, 24, 167, 54, 104, 83, 48, 62, 24, 47 };
-
-			//for (int i = 0; i < sur1D.Length; i++)
-			//{
-			//	GpsPos pozicia1 = new GpsPos('N', sur1S[i], 'E', sur1D[i]);
-			//	GpsPos pozicia2 = new GpsPos('N', sur2S[i], 'E', sur2D[i]);
-			//	Nehnutelnost nehnutelnost = new Nehnutelnost();
-			//	nehnutelnost.Pozicie[0] = pozicia1;
-			//	nehnutelnost.Pozicie[1] = pozicia2;
-			//	nehnutelnosti.Insert(nehnutelnost);
-			//	//if (i == 4)
-			//	//{
-			//	//	break;
-			//	//}
-			//}
-
-			Console.WriteLine(nehnutelnosti.Count);
+			Console.WriteLine("Celkový počet objektov v strome: " + objekty.Count);
 			while (true)
 			{
 				var key = Console.ReadKey();
