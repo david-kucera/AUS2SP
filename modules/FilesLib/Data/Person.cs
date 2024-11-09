89cea1433c757757139ac8bef5d6073628a539cb
david-kucera <kucera.david2002@gmail.com>
2024-11-09 11:03:53 +0100

    Návrh štruktúr

---
+++ modules/FilesLib/Data/Person.cs
@@ -0,0 +1,43 @@
+﻿namespace FilesLib.Data
+{
+	public class Person
+	{
+		#region Constants
+		private const int MAX_VISITS = 5;
+		private const int MAX_NAME_LENGTH = 15;
+		private const int MAX_SURNAME_LENGTH = 20;
+		#endregion // Constants
+
+		#region Properties
+		public string Name { get; set; } = string.Empty;
+		public string Surname { get; set; } = string.Empty;
+		public int Id { get; set; } = int.MinValue;
+		public Visit[] Zaznamy { get; set; } = new Visit[MAX_VISITS];
+		#endregion // Properties
+
+		#region Constructors
+		public Person(int id, string name, string surname)
+		{
+			if (name.Length > MAX_NAME_LENGTH)
+			{
+				throw new ArgumentException($"Name is too long. Max length is {MAX_NAME_LENGTH}.");
+			}
+			if (surname.Length > MAX_SURNAME_LENGTH)
+			{
+				throw new ArgumentException($"Surname is too long. Max length is {MAX_SURNAME_LENGTH}.");
+			}
+			
+			Name = name;
+			Surname = surname;
+			Id = id;
+		}
+		#endregion // Constructors
+
+		#region Public functions
+		public override string ToString()
+		{
+			return $"[{Id}] {Name} {Surname} ";
+		}
+		#endregion // Public functions
+	}
+}
---
+++ modules/FilesLib/Data/Visit.cs
@@ -0,0 +1,37 @@
+﻿namespace FilesLib.Data
+{
+	public class Visit
+	{
+		#region Constants
+		private const int MAX_NOTE_LENGTH = 20;
+		#endregion // Constants
+
+		#region Properties
+		public DateTime Date { get; set; } = DateTime.Now;
+		public double Price { get; set; } = double.MinValue;
+		public string Note { get; set; } = string.Empty;
+		#endregion // Properties
+
+		#region Constructors
+		public Visit(DateTime date, double price, string note)
+		{
+			if (note.Length > MAX_NOTE_LENGTH)
+			{
+				throw new ArgumentException($"Note length must be less than {MAX_NOTE_LENGTH} characters.");
+			}
+
+			Date = date;
+			Price = price;
+			Note = note;
+		}
+		#endregion // Constructors
+
+		#region Public functions
+		public override string ToString()
+		{
+			return $"Date: {Date}, Price: {Price}, Note: {Note}";
+		}
+		#endregion // Public functions
+
+	}
+}
