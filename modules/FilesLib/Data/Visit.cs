4e538eb52b9fa6acd0d4006870320bc7fe5d5a7d
david-kucera <kucera.david2002@gmail.com>
2024-11-15 17:51:42 +0100

    Oprava serializácie triedy Visit

--- modules/FilesLib/Data/Visit.cs
+++ modules/FilesLib/Data/Visit.cs
@@ -6,12 +6,25 @@ namespace FilesLib.Data
 	{
 		#region Constants
 		private const int MAX_NOTE_LENGTH = 20;
+		private const double TOLERANCE = 0.0001;
 		#endregion // Constants
 
 		#region Properties
 		public DateOnly Date { get; set; } = DateOnly.MinValue;
 		public double Price { get; set; } = double.MinValue;
-		public string Note { get; set; } = string.Empty;
+		private string _note = string.Empty;
+		public string Note
+		{
+			get => _note;
+			set
+			{
+				if (value.Length > MAX_NOTE_LENGTH)
+				{
+					throw new ArgumentException($"Note length must not exceed {MAX_NOTE_LENGTH} characters.");
+				}
+				_note = value;
+			}
+		}
 		#endregion // Properties
 
 		#region Constructors
@@ -29,7 +42,7 @@ namespace FilesLib.Data
 
 			Date = date;
 			Price = price;
-			Note = note;
+			_note = note;
 		}
 		#endregion // Constructors
 
@@ -43,8 +56,8 @@ namespace FilesLib.Data
         {
             int ret = 0;
             ret += sizeof(double);	// Price
-            ret += 3 * sizeof(int); // Date
-            ret += MAX_NOTE_LENGTH * sizeof(char); // Note
+            ret += sizeof(ushort) + sizeof(byte) + sizeof(byte); // Date
+			ret += MAX_NOTE_LENGTH * sizeof(char); // Note
             return ret;
         }
 
@@ -55,7 +68,7 @@ namespace FilesLib.Data
 
         public bool Equals(Visit data)
         {
-            return Date == data.Date && Price == data.Price && Note == data.Note;
+            return Date == data.Date && Math.Abs(Price - data.Price) < TOLERANCE && Note == data.Note;
         }
 
         public byte[] ToByteArray()
@@ -63,25 +76,65 @@ namespace FilesLib.Data
 			byte[] bytes = new byte[GetSize()];
             int offset = 0;
 
-            // TODO check dateonly size
-            bytes.CopyTo(BitConverter.GetBytes(Price), offset);
+			// 8 bytes for double
+			bytes.CopyTo(BitConverter.GetBytes(Price), offset);
             offset += sizeof(double);
-            bytes.CopyTo(BitConverter.GetBytes(Date.Year), offset);
-            offset += sizeof(int);
-            bytes.CopyTo(BitConverter.GetBytes(Date.Month), offset);
-            offset += sizeof(int);
-            bytes.CopyTo(BitConverter.GetBytes(Date.Day), offset);
-            offset += sizeof(int);
-            bytes.CopyTo(Encoding.ASCII.GetBytes(Note), offset);
-            offset += MAX_NOTE_LENGTH * sizeof(char);
+
+            // 2 bytes for Year
+            BitConverter.GetBytes((ushort)Date.Year).CopyTo(bytes, offset);
+            offset += sizeof(ushort);
+
+			// 1 byte for Month
+			bytes[offset] = (byte)Date.Month;
+			offset++;
+
+			// 1 byte for Day
+			bytes[offset] = (byte)Date.Day;
+			offset++;
+
+			// 4 bytes for note length and 20 bytes for note chars
+			int noteLength = Note.Length;
+			bytes.CopyTo(BitConverter.GetBytes(noteLength), offset);
+			offset += sizeof(int);
+
+			if (noteLength < MAX_NOTE_LENGTH)
+			{
+				Note = Note.PadRight(MAX_NOTE_LENGTH, ' ');
+			}
+			bytes.CopyTo(Encoding.ASCII.GetBytes(Note), offset);
 
             return bytes;
         }
 
         public Visit FromByteArray(byte[] byteArray)
         {
-            throw new NotImplementedException();
-        }
+            int offset = 0;
+
+			// Price
+			double price = BitConverter.ToDouble(byteArray, offset);
+			offset += sizeof(double);
+
+			// Year
+			ushort year = BitConverter.ToUInt16(byteArray, offset);
+			offset += sizeof(ushort);
+
+			// Month
+			byte month = byteArray[offset];
+			offset++;
+
+			// Day
+			byte day = byteArray[offset];
+			offset++;
+
+			// Note length
+			int noteLength = BitConverter.ToInt32(byteArray, offset);
+			offset += sizeof(int);
+
+			// Note
+			string note = Encoding.ASCII.GetString(byteArray, offset, noteLength);
+
+			return new Visit(new DateOnly(year, month, day), price, note);
+		}
         #endregion // Public functions
     }
 }
