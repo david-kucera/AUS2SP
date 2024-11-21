5bac879035121e3a3479decfbd288442003efda0
david-kucera <kucera.david2002@gmail.com>
2024-11-21 17:18:26 +0100

    Odladenie a dokončenie heap file

--- modules/FilesLib/HeapFile.cs
+++ modules/FilesLib/HeapFile.cs
@@ -5,8 +5,6 @@
 		#region Class members
 		private FileStream _file;
 		private string _initFilePath;
-		private Block<T> _currentBlock = null!;
-        private int _currentBlockAddress = 0;
 		private int _nextFreeBlockAddress = -1;
 		private int _nextEmptyBlockAddress = -1;
         #endregion // Class members
@@ -14,8 +12,6 @@
         #region Properties
         public int BlockSize { get; set; }
 		public int BlockCount => (int)(_file.Length / BlockSize);
-		public int NextFreeBlockAddress => _nextFreeBlockAddress;
-		public int NextEmptyBlockAddress => _nextEmptyBlockAddress;
 		#endregion // Properties
 
 		#region Constructors
@@ -57,124 +53,129 @@
         #region Public functions
         public int Insert(T data)
 		{
-            if (_nextFreeBlockAddress != -1)
+			// Ak mam nejake volne blocky na pridanie dat
+            if (_nextFreeBlockAddress != -1 || _nextEmptyBlockAddress != -1)
             {
-                SetCurrentBlock(_nextFreeBlockAddress);
-                _currentBlock.AddRecord(data);
+	            // najskor vyberam ciastocne plne, ak ciastocne plne nie su, tak potom celkom prazdne
+	            int address;
+	            if (_nextFreeBlockAddress != -1) address = _nextFreeBlockAddress;
+	            else address = _nextEmptyBlockAddress;
 
-                if (_currentBlock.ValidCount == _currentBlock.BlockFactor)
-                {
-	                if (_currentBlock.Next != -1)
-	                {
-		                var nextBlock = GetBlock(_currentBlock.Next);
-		                nextBlock.Previous = -1;
-		                WriteBlock(nextBlock, _currentBlock.Next);
-	                }
-	                
-	                _nextFreeBlockAddress = _currentBlock.Next;
-	                _currentBlock.Next = -1;
-	                //_currentBlock.Previous = -1;
-                }
+	            var blockToAdd = GetBlock(address);
+	            blockToAdd.AddRecord(data);
+
+	            if (blockToAdd.ValidCount < blockToAdd.BlockFactor) // ak sa do neho este zmesti
+	            {
+		            if (_nextEmptyBlockAddress == address)
+		            {
+			            _nextEmptyBlockAddress = blockToAdd.Next;
+			            if (blockToAdd.Next != -1)
+			            {
+				            var nextBlock = GetBlock(blockToAdd.Next);
+				            nextBlock.Previous = -1;
+				            WriteBlock(nextBlock, blockToAdd.Next);
+			            }
+			            blockToAdd.Next = _nextFreeBlockAddress;
+			            if (_nextFreeBlockAddress != -1)
+			            {
+				            var nextFreeBlock = GetBlock(_nextFreeBlockAddress);
+				            nextFreeBlock.Previous = address;
+				            WriteBlock(nextFreeBlock, _nextFreeBlockAddress);
+			            }
+			            _nextFreeBlockAddress = address;
+		            }
+	            }
+	            else if (blockToAdd.ValidCount == blockToAdd.BlockFactor) // ak je blok plny
+	            {
+		            if (blockToAdd.Next != -1)
+		            {
+			            var nextBlock = GetBlock(blockToAdd.Next);
+			            nextBlock.Previous = -1;
+			            WriteBlock(nextBlock, blockToAdd.Next);
+		            }
+
+		            if (_nextEmptyBlockAddress == address)
+		            {
+			            _nextEmptyBlockAddress = blockToAdd.Next;
+			            blockToAdd.Next = -1;
+		            }
+		            else if (_nextFreeBlockAddress == address)
+		            {
+			            _nextFreeBlockAddress = blockToAdd.Next;
+			            blockToAdd.Next = -1;
+		            }
+	            }
+	            else
+	            {
+		            throw new Exception("Error while inserting data");
+	            }
+	            
+	            WriteBlock(blockToAdd, address);
+	            return address;
             }
-            else if (_nextEmptyBlockAddress != -1)
+            else // vytvaram novy blok na koniec suboru
             {
-                SetCurrentBlock(_nextEmptyBlockAddress);
-                _currentBlock.AddRecord(data);
+	            var address = (int)_file.Length;
+	            var newBlock = new Block<T>(BlockSize, new T());
+                newBlock.AddRecord(data);
 
-                if (_currentBlock.ValidCount == _currentBlock.BlockFactor)
+                if (newBlock.ValidCount < newBlock.BlockFactor) 
                 {
-	                if (_currentBlock.Next != -1)
-	                {
-		                var nextBlock = GetBlock(_currentBlock.Next);
-		                nextBlock.Previous = -1;
-		                WriteBlock(nextBlock, _currentBlock.Next);
-	                }
-	                
-	                _nextEmptyBlockAddress = _currentBlock.Next;
-	                _currentBlock.Next = -1;
-	                //_currentBlock.Previous = -1;
-                }
-                else
-                {
-	                _nextEmptyBlockAddress = _currentBlock.Next;
-	                if (_currentBlock.Next != -1)
-	                {
-		                var nextBlock = GetBlock(_currentBlock.Next);
-		                nextBlock.Previous = -1;
-		                WriteBlock(nextBlock, _currentBlock.Next);
-	                }
-	                
-	                _currentBlock.Next = _nextFreeBlockAddress;
-	                if (_nextFreeBlockAddress != -1)
+	                if (_nextFreeBlockAddress != -1) // zaradujem do zretazenia ciastocne plnych blokov
 	                {
 		                var nextBlock = GetBlock(_nextFreeBlockAddress);
-		                nextBlock.Previous = _currentBlockAddress;
+		                nextBlock.Previous = address;
+		                newBlock.Next = _nextFreeBlockAddress;
 		                WriteBlock(nextBlock, _nextFreeBlockAddress);
 	                }
-	                _nextFreeBlockAddress = _currentBlockAddress;
+	                _nextFreeBlockAddress = address;
                 }
+                WriteBlock(newBlock, address);
+                return address;
             }
-            else
-            {
-                SetCurrentBlock((int)_file.Length);
-                _currentBlock.AddRecord(data);
-
-                if (_currentBlock.ValidCount < _currentBlock.BlockFactor) // zaradujem do zretazenia ciastocne plnych blokov
-                {
-	                if (_nextFreeBlockAddress != -1)
-	                {
-		                var nextBlock = GetBlock(_nextFreeBlockAddress);
-		                nextBlock.Previous = _currentBlockAddress;
-		                WriteBlock(nextBlock, _nextFreeBlockAddress);
-		                _currentBlock.Next = _nextFreeBlockAddress;
-	                }
-	                _nextFreeBlockAddress = _currentBlockAddress;
-                }
-            }
-            
-            WriteBlock(_currentBlock, _currentBlockAddress);
-            return _currentBlockAddress;
         }
 
         public T Find(int address, T data)
         {
-	        if (address % BlockSize != 0 || address < 0 || address > _file.Length) throw new ArgumentException("Invalid address");
-	        
-            SetCurrentBlock(address);
-            return _currentBlock.GetRecord(data);
+	        CheckAddress(address);
+	        return GetBlock(address).GetRecord(data);
         }
 
         public bool Delete(int address, T data)
-		{
-			if (address % BlockSize != 0 || address < 0 || address > _file.Length) throw new ArgumentException("Invalid address");
+        {
+	        CheckAddress(address);
 
 			var blockToDeleteFrom = GetBlock(address);
 			if (!blockToDeleteFrom.RemoveRecord(data)) return false;
 			
-			if (blockToDeleteFrom.Next == -1 && blockToDeleteFrom.Previous == -1)
+			if (blockToDeleteFrom is { Next: -1, Previous: -1 })
 			{
 				if (blockToDeleteFrom.ValidCount > 0)
 				{
-					if (_nextFreeBlockAddress != -1)
+					if (_nextFreeBlockAddress != address)
 					{
-						var nextFreeBlock = GetBlock(_nextFreeBlockAddress);
-						nextFreeBlock.Next = address;
-						blockToDeleteFrom.Previous = _nextFreeBlockAddress;
-						WriteBlock(nextFreeBlock, _nextFreeBlockAddress);
+						if (_nextFreeBlockAddress != -1)
+						{
+							var nextFreeBlock = GetBlock(_nextFreeBlockAddress);
+							nextFreeBlock.Previous = address;
+							blockToDeleteFrom.Next = _nextFreeBlockAddress;
+							WriteBlock(nextFreeBlock, _nextFreeBlockAddress);
+						}
+						_nextFreeBlockAddress = address;
 					}
-					_nextFreeBlockAddress = address;
 				}
 				else
 				{
 					if (_nextEmptyBlockAddress != -1)
 					{
 						var nextEmptyBlock = GetBlock(_nextEmptyBlockAddress);
-						nextEmptyBlock.Next = address;
-						blockToDeleteFrom.Previous = _nextEmptyBlockAddress;
+						nextEmptyBlock.Previous = address;
+						blockToDeleteFrom.Next = _nextEmptyBlockAddress;
 						WriteBlock(nextEmptyBlock, _nextEmptyBlockAddress);
 					}
 					_nextEmptyBlockAddress = address;
-					if (_nextFreeBlockAddress != address)
+					
+					if (_nextFreeBlockAddress == address)
 					{
 						_nextFreeBlockAddress = -1;
 					}
@@ -188,6 +189,11 @@
 					{
 						var nextBlock = GetBlock(blockToDeleteFrom.Next);
 						nextBlock.Previous = blockToDeleteFrom.Previous;
+						if (_nextFreeBlockAddress == address)
+						{
+							_nextFreeBlockAddress = blockToDeleteFrom.Next;
+							nextBlock.Previous = -1;
+						}
 						WriteBlock(nextBlock, blockToDeleteFrom.Next);
 					}
 
@@ -204,8 +210,8 @@
 					if (_nextEmptyBlockAddress != -1)
 					{
 						var nextEmptyBlock = GetBlock(_nextEmptyBlockAddress);
-						nextEmptyBlock.Next = address;
-						blockToDeleteFrom.Previous = _nextEmptyBlockAddress;
+						nextEmptyBlock.Previous = address;
+						blockToDeleteFrom.Next = _nextEmptyBlockAddress;
 						WriteBlock(nextEmptyBlock, _nextEmptyBlockAddress);
 					}
 					_nextEmptyBlockAddress = address;
@@ -216,21 +222,10 @@
 			CheckFileEnding();
 			return true;
 		}
-
-		public void Clear()
-		{
-			_currentBlock.ResetBlock();
-            _currentBlockAddress = 0;
-            _nextFreeBlockAddress = -1;
-            _nextEmptyBlockAddress = -1;
-
-			SaveInitData();
-			_file.SetLength(BlockSize);
-            _file.Flush();
-        }
-
+        
 		public Block<T> GetBlock(int address)
 		{
+			CheckAddress(address);
 			_file.Seek(address, SeekOrigin.Begin);
 			byte[] bytes = new byte[BlockSize];
 			_file.Read(bytes, 0, BlockSize);
@@ -239,7 +234,7 @@
 			return block;
 		}
 		
-        public List<Block<T>> GetBlocks()
+        public List<Block<T>> GetAllBlocks()
         {
 	        var ret = new List<Block<T>>();
 	        for (int i = 0; i < BlockCount; i++)
@@ -253,15 +248,42 @@
 		        
 		        ret.Add(block);
 	        }
-
 	        return ret;
         }
 
+        public List<T> GetAllRecords()
+        {
+	        var ret = new List<T>();
+	        for (int i = 0; i < BlockCount; i++)
+	        {
+		        Block<T> block = new Block<T>(BlockSize, new T());
+		        int offset = i * BlockSize;
+		        _file.Seek(offset, SeekOrigin.Begin);
+		        byte[] bytes = new byte[BlockSize];
+		        _file.Read(bytes, 0, BlockSize);
+		        block.FromByteArray(bytes);
+
+		        foreach (var record in block.Records)
+		        {
+			        ret.Add(record);
+		        }
+	        }
+	        return ret;
+        }
+
+        public void Clear()
+        {
+	        _nextFreeBlockAddress = -1;
+	        _nextEmptyBlockAddress = -1;
+
+	        SaveInitData();
+	        _file.SetLength(BlockSize);
+	        _file.Flush();
+        }
+        
         public void Close()
         {
 	        SaveInitData();
-	        
-	        _currentBlockAddress = 0;
 	        _nextFreeBlockAddress = -1;
 	        _nextEmptyBlockAddress = -1;
 	        _file.Flush();
@@ -300,27 +322,6 @@
 	        _nextEmptyBlockAddress = BitConverter.ToInt32(buffer, offset);
         }
         
-        private void SetCurrentBlock(int address)
-        {
-	        if (address % BlockSize != 0 || address < 0 || address > _file.Length) throw new ArgumentException("Invalid address");
-	        
-	        if (_currentBlock == null! || _currentBlockAddress != address)
-	        {
-		        _currentBlock = new Block<T>(BlockSize, new T());
-		        byte[] bytes = new byte[BlockSize];
-		        _file.Seek(address, SeekOrigin.Begin);
-		        _file.Read(bytes, 0, BlockSize);
-		        _currentBlock.FromByteArray(bytes);
-		        _currentBlockAddress = address;
-		        
-		        if (address == _file.Length)
-		        {
-			        _currentBlock.Next = -1;
-			        _currentBlock.Previous = -1;
-		        }
-	        }
-        }
-        
         private void WriteBlock(Block<T> currentBlock, int currentBlockAddress)
         {
 	        byte[] bytes = currentBlock.ToByteArray();
@@ -338,25 +339,39 @@
 
 		        if (lastBlock.ValidCount > 0) break;
 		        
-		        if (lastBlock.Next != -1 /*&& lastBlock.Next < _file.Length && lastBlock.Previous < _file.Length*/)
+		        if (lastBlock.Next != -1 && lastBlock.Next < _file.Length && lastBlock.Previous < _file.Length)
 		        {
 			        var nextBlock = GetBlock(lastBlock.Next);
 			        nextBlock.Previous = lastBlock.Previous;
 			        WriteBlock(nextBlock, lastBlock.Next);
+			        if (_nextEmptyBlockAddress == lastBlockAddress)
+			        {
+				        _nextEmptyBlockAddress = lastBlock.Next;
+				        nextBlock.Previous = -1;
+			        }
+		        }
+		        else
+		        {
+			        if (_nextEmptyBlockAddress == lastBlockAddress)
+			        {
+				        _nextEmptyBlockAddress = -1;
+			        }
 		        }
 
-		        if (lastBlock.Previous != -1 /*&& lastBlock.Previous < _file.Length && lastBlock.Next < _file.Length*/)
+		        if (lastBlock.Previous != -1 && lastBlock.Previous < _file.Length && lastBlock.Next < _file.Length)
 		        {
 			        var prevBlock = GetBlock(lastBlock.Previous);
 			        prevBlock.Next = lastBlock.Next;
 			        WriteBlock(prevBlock, lastBlock.Previous);
 		        }
-
-		        if (_nextFreeBlockAddress == lastBlockAddress) _nextFreeBlockAddress = lastBlock.Next;
-		        if (_nextEmptyBlockAddress == lastBlockAddress) _nextEmptyBlockAddress = lastBlock.Next;
 		        
 		        _file.SetLength(_file.Length - BlockSize);
 	        }
+        }
+        
+        private void CheckAddress(int address)
+        {
+	        if (address % BlockSize != 0 || address < 0 || address > _file.Length) throw new ArgumentException("Invalid address");
         }
         #endregion // Private functions
     }
--- modules/FilesTest/Program.cs
+++ modules/FilesTest/Program.cs
@@ -5,14 +5,14 @@ namespace FilesTest
 {
     public class Program
     {
-        public static int BLOCK_SIZE = 1024;
+        public static int BLOCK_SIZE = 1024 * 4;
         public static string INIT_FILE = "../../userdata/person_init.aus";
         public static string DATA_FILE = "../../userdata/person.aus";
-        public static int NUMBER_OF_PEOPLE = 1000;
+        public static int NUMBER_OF_PEOPLE = 0;
         public static int NUMBER_OF_REPLICATIONS = 10;
         static void Main()
         {
-            int numberOperations = 10_000;
+            int numberOperations = 100_000;
             for (int r = 0; r < NUMBER_OF_REPLICATIONS; r++)
             {
                 if (File.Exists(DATA_FILE)) File.Delete(DATA_FILE);
@@ -20,8 +20,8 @@ namespace FilesTest
 
                 HeapFile<TestRP1> heapFile = new(INIT_FILE, DATA_FILE, BLOCK_SIZE);
                 DataGenerator generator = new DataGenerator(r);
-                List<int> adresses = new(NUMBER_OF_PEOPLE);
-                List<TestRP1> people = new(NUMBER_OF_PEOPLE);
+                List<int> adresses = new();
+                List<TestRP1> people = new();
                 
                 for (int x = 0; x < NUMBER_OF_PEOPLE; x++)
                 {
@@ -45,6 +45,7 @@ namespace FilesTest
                             adresses.Add(adresa);
                             break;
                         case OperationType.Delete:
+                            if (adresses.Count == 0) break;
                             var index = generator.GenerateInt(0, adresses.Count);
                             var person = people[index];
                             var adress = adresses[index];
@@ -53,6 +54,7 @@ namespace FilesTest
                             heapFile.Delete(adress, person);
                             break;
                         case OperationType.Find:
+                            if (adresses.Count == 0) break;
                             var iindex = generator.GenerateInt(0, adresses.Count);
                             var pperson = people[iindex];
                             var aadress = adresses[iindex];
@@ -66,69 +68,9 @@ namespace FilesTest
                             break;
                     }
                 }
-                
-                // for (int i = 0; i < x; i++)
-                // {
-                //     var index = generator.GenerateInt(0, adresses.Count);
-                //     var person = people[index];
-                //     var adress = adresses[index];
-                //     people.RemoveAt(index);
-                //     adresses.RemoveAt(index);
-                //     heapFile.Delete(adress, person);
-                // }
             
                 heapFile.Close();
             }
-            
-            
-
-            // Console.WriteLine("Number of blocks:");
-            // Console.WriteLine(heapFile.GetBlocks().Count);
-            // Console.WriteLine("------------------------");
-
-            // for (int i = 0; i < NUMBER_OF_PEOPLE; i++)
-            // {
-            //     var pers = heapFile.Find(adresses[i], people[i]);
-            //     if (pers == null) throw new Exception("Data not found!");
-            // }
-            
-            // Console.WriteLine("All blocks:");
-            // var blocks = heapFile.GetBlocks();
-            // for (int i = 0; i < blocks.Count; i++)
-            // {
-            //     Console.WriteLine($"{i + 1}. Block {blocks[i]}");
-            // }
-
-            // Console.WriteLine(heapFile.NextEmptyBlockAddress);
-            // Console.WriteLine(heapFile.NextFreeBlockAddress);
-            
-            // for (int i = NUMBER_OF_PEOPLE-1; i >= 0; i--)
-            // {
-            //     heapFile.Delete(adresses[i], people[i]);
-            //     Console.WriteLine($"{i}. Deleted person");
-            //     Console.WriteLine(heapFile.GetBlocks().Count);
-            // }
-            
-            // for (int i = 0; i < NUMBER_OF_PEOPLE; i++)
-            // {
-            //     heapFile.Delete(adresses[i], people[i]);
-            //     Console.WriteLine($"{i}. Deleted person");
-            //     Console.WriteLine(heapFile.GetBlocks().Count);
-            // }
-            
-            // var allBlocks = heapFile.GetBlocks();
-            // Console.WriteLine("------------------------");
-            // Console.WriteLine("All Blocks");
-            // int poradie = 1;
-            // foreach (var b in allBlocks)
-            // {
-            //     Console.WriteLine("------------------------");
-            //     Console.WriteLine(poradie + ". block\n" + b);
-            //     Console.WriteLine("------------------------");
-            //     poradie++;
-            // }
-            
-            
         }
     }
 }
