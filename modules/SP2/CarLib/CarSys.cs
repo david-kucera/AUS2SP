b6455280edf3c5f58a315979eef0179564c80f95
david-kucera <kucera.david2002@gmail.com>
2024-11-24 14:21:32 +0100

    Vytvorenie a konfigurácia projektov pre GUI a logiku výslednej aplikácie

---
+++ modules/SP2/CarLib/CarSys.cs
@@ -0,0 +1,37 @@
+﻿using FilesLib.Data;
+
+namespace CarLib
+{
+	public class CarSys : ICar
+	{
+		public Person Find(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Person Find(string ecv)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Add(Person person)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void AddVisit(Person person)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RemoveVisit(Person person)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Remove(Person person)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ modules/SP2/CarLib/ICar.cs
@@ -0,0 +1,14 @@
+﻿using FilesLib.Data;
+
+namespace CarLib
+{
+	public interface ICar
+	{
+		Person Find(int id);
+		Person Find(string ecv);
+		void Add(Person person);
+		void AddVisit(Person person);
+		void RemoveVisit(Person person);
+		void Remove(Person person);
+	}
+}
